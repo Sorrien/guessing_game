bc8246f0433658fe7e19bb1802f1931d8d6c3913
Sorrien <collin.sparks@gmail.com>
2018-11-18 13:19:44 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    loop {
+        println!("Guess the number!");
+        println!("Please input your guess.");
+
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+
+        loop {
+            let mut guess = String::new();
+
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => { 
+                    println!("Please enter a number!");
+                    continue;
+                }
+            };
+
+            println!("You guessed: {}", guess);
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
+            }
+        }
+
+        println!("Would you like to play again? (y/n)");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let chars: Vec<char> = input.to_lowercase().chars().collect();
+        if chars[0] == 'n' {
+            break;
+        }
+    }
+}
